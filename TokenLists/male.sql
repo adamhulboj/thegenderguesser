33e4c19c92e2400ab116c795f8a0836c142392b0
adam.hulboj <adam.hulboj@gmail.com>
2021-08-20 13:40:46 +0200

    SQL to firebird database - give me some male and female names

---
+++ TokenLists/male.sql
@@ -0,0 +1,16 @@
+/*male*/
+SELECT DISTINCT a.name
+FROM
+(
+SELECT 
+UPPER(TRIM(o.simie)) AS name,
+MOD(CAST( SUBSTRING(o.spesel FROM 10 FOR 1) AS integer),2) AS gender   
+FROM r_osoby o
+WHERE 
+	o.spesel IS NOT NULL 
+	AND	CHAR_LENGTH(o.spesel) = 11 
+	AND	SUBSTRING(TRIM(o.spesel) FROM 10 FOR 1) SIMILAR TO '[0-9]+' 
+	-- AND SUBSTRING(o.spesel FROM 10 FOR 1) <> ''
+) A 
+WHERE 
+ a.gender = 1
---
+++ TokenLists/readme.txt
@@ -0,0 +1,6 @@
+create base of male and female tokens (from existing data)
+
+we can generate this having list of personal data name surname and pesel, last digit of pesel give us information what gender the person have
+I can have that kind of list analizyng database in current company I will only give Name and Genger, no personal data or pesel will be leaked
+
+SQL's to Firebird database
