62221ff839994158500dedcb3800b582a18f603d
adam.hulboj <adam.hulboj@gmail.com>
2021-08-23 20:39:43 +0200

    first hello world app

---
+++ genderguesser/src/test/java/com/example/genderguesser/GenderguesserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genderguesser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderguesserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
