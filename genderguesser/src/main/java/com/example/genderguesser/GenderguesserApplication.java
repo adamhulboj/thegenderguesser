5a80c6bee9a8b8a5ab7181c102bdd8bfd7347009
adam.hulboj <adam.hulboj@gmail.com>
2021-08-24 11:22:29 +0200

    maven project - first success

--- genderguesser/src/main/java/com/example/genderguesser/GenderguesserApplication.java
+++ genderguesser/src/main/java/com/example/genderguesser/GenderguesserApplication.java
@@ -7,13 +7,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GenderguesserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GenderguesserApplication.class, args);
 	}
+
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+
+	@GetMapping("hello2")
+	public String hello2() {
+		return "czesc";
+	}
+
+	@GetMapping("/error")
+	public String err() {
+		return "somfin went wong";
+	}
+
+
 }
